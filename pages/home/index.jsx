635f172b32ee40962e07ed1f7e5aff8a92ed8238
Rharuow <haryssonsoares@gmail.com>
2020-08-28 10:57:41 -0300

    style: refactor structure section

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { signOut, useSession } from 'next-auth/client'
 
 import Header from '../../components/common/Header'
+import Section from '../../components/Section'
 
 const Home =() => {
     const [ session, loading ] = useSession()
@@ -12,7 +13,9 @@ const Home =() => {
         <div className="main">
             <Header name={session.user.name} img={session.user.image} closeSession={signOut}/>
 
-            
+            <div className="spends mt-5">
+                <Section name="Despesas" icon="fas fa-house-user" title="aluguel"/>
+            </div>
         </div>
     )
 }
