319f3d512b9e8d94910d7835649ab7696e246c43
Rharuow <haryssonsoares@gmail.com>
2020-09-09 17:20:41 -0300

    WIP: generic component

---
+++ components/Debts/Body/Generic.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Generic = ({ info }) => {
+  return (
+    <>
+      <Header title={info.title} />
+      <div className="board-body mt-2">
+        <ul className="list-group">
+          <li className="list-group-item list-group-item-dark d-flex justify-content-between">Total: <strong className="text-center">R$ {formatMoney(info.totalValue)}</strong></li>
+          <li className="list-group-item list-group-item-dark d-flex justify-content-between">Sua parte: <strong className="text-center">R$ {formatMoney(info.subTotal)}</strong></li>
+          <li className="list-group-item list-group-item-dark d-flex justify-content-between">Pagar para: <strong className="text-center">{info.own}</strong></li>
+          <li className="list-group-item list-group-item-dark d-flex justify-content-between">Data da dívida: <strong className="text-center">{info.createdAt}</strong></li>
+          <li className="list-group-item list-group-item-dark d-flex justify-content-between">Vencimento: <strong className="text-center">{info.dueDate === "" ? "--/--/--" : info.dueDate}</strong></li>
+          <li className="list-group-item list-group-item-dark d-flex justify-content-between">Status: <span className={`bge bge-${info.status}`}>{status(info.status)}</span></li>
+        </ul>
+      </div>
+      <Footer participants={info.participants} own={info.own} />
+    </>
+  )
+}
+
+Generic.propTypes = {
+  info: PropTypes.object
+}
+
+export default Generic
