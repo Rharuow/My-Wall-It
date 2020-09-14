5434c31d65135e4daf53c61576d78e5580068d2d
Rharuow <haryssonsoares@gmail.com>
2020-09-14 06:41:57 -0300

    WIP: select component

--- components/Form/index.jsx
+++ components/Form/index.jsx
@@ -1,17 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 
 import { debtName } from '../../assets/scripts/utils/translate'
 
 const Form = ({ params }) => {
+
   return (
     <form action="">
-      <div className="form-group">
-        <label htmlFor="exampleFormControlSelect1">Selecione um tipo de dívida:</label>
-        <select className="form-control" id="exampleFormControlSelect1">
-          {params.map((param, index) => <option key={`option${index}`}>{debtName(param.title)}</option>)}
-        </select>
-      </div>
+
     </form>
   )
 }
