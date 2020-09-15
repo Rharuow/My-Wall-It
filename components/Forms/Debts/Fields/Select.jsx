605dd7a55c62203a42cb007c81dbc9defc710e6f
Rharuow <haryssonsoares@gmail.com>
2020-09-14 22:52:30 -0300

    chore: debtName componenet

--- components/Forms/Debts/Fields/Select.jsx
+++ components/Forms/Debts/Fields/Select.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { debtName } from '../../../../assets/scripts/utils/translate'
-
 const Select = ({ className, classNameOptions, onChange, id, labelText, options, selected }) => {
   return (
     <>
