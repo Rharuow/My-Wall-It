d795a742152e0b7c9ef638fa195408646c7461a3
Rharuow <haryssonsoares@gmail.com>
2020-09-14 23:11:08 -0300

    chore: session flag

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -23,7 +23,7 @@ const Home = () => {
 
   return (
     <div className="main">
-      <Header name={session.user.name} img={session.user.image} closeSession={signOut} />
+      <Header name={session ? session.user.name : "Harysson Soares"} img={session ? session.user.image : "https://icons-for-free.com/iconfiles/png/512/business+costume+male+man+office+user+icon-1320196264882354682.png"} closeSession={signOut} />
       <div className="section">
         <h3 className="section-title">Despesas atuais</h3>
         <Slider {...settings}>
