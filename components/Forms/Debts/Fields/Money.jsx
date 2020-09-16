1df09c26ccd573cf48d6ed5d46d1a93a66bb0158
Rharuow <haryssonsoares@gmail.com>
2020-09-16 00:20:47 -0300

    feat: input component house name

---
+++ components/Forms/Debts/Fields/Money.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Money = props => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+Money.propTypes = {
+
+}
+
+export default Money
